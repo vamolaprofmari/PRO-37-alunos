4b3d701fd9ff75fe8cbd1da49315f723d749353a
vamolaprofmari <123978185+vamolaprofmari@users.noreply.github.com>
2023-05-15 19:52:50 -0300

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,39 @@
+class Form {
+    constructor() {
+      this.input = createInput("").attribute("placeholder", "Enter your name");
+      this.playButton = createButton("Play");
+      this.titleImg = createImg("./assets/title.png", "game title");
+      this.greeting = createElement("h2");
+    }
+  
+    setElementsPosition() {
+      this.titleImg.position(120, 100);
+      this.input.position(width / 2 - 110, height / 2 - 80);
+      this.playButton.position(width / 2 - 90, height / 2 - 20);
+      this.greeting.position(width / 2 - 300, height / 2 - 100);
+    }
+
+    hide(){
+        this.greeting.hide();
+        this.playButton.hide();
+        this.input.hide();
+
+    }
+    handleMousePressed(){
+        this.playButton.mousePressed(() => {
+            this.input.hide();
+            this.playButton.hide();
+            var message = `
+      Olá ${this.input.value()}
+      </br>Esperando outro jogador entrar para jogar...`;
+            this.greeting.html(message);
+            player.name = this.input.value();
+            player.index = 1;
+        });
+    }
+
+    display(){
+        this.setElementsPosition();
+        this.handleMousePressed();
+    }
+}
